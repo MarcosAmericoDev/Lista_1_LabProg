461c2279edfcc1e1df2f063e0cf00d34f63c13d3
Marcos Iury de Lima Américo <MarcosIuryProgramador@gmail.com>
2023-02-14 19:21:43 -0300

    Adição da questão 22

---
+++ 22.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("A: %d \n", 'A');
+    printf("B: %d \n", 'B');
+    printf("C: %d \n", 'C');
+    printf("a: %d \n", 'a');
+    printf("b: %d \n", 'b');
+    printf("c: %d \n", 'c');
+    printf("0: %d \n", '0');
+    printf("12: %d \n", '12');
+    printf("$: %d \n", '$');
+    printf("*: %d \n", '*');
+    printf("+: %d \n", '+');
+    printf("/: %d \n", '/');
+    printf("Espaço vazio: %d \n", ' ');
+
+    return 0;
+}
